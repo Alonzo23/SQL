9cc7bee9fd010ef953c9720ab1ee72ba53136292
Alonzo23 <49086724+Alonzo23@users.noreply.github.com>
2019-08-03 21:44:48 -0500

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -6,9 +6,6 @@ employees,
 salaries,
 titles;
 
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
